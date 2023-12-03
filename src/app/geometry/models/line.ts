030b7792c4450976da111f9edf4beb1f0b0f928d
Twazny <wazny.tomek@gmail.com>
2023-12-03 19:38:29 +0100

    feat(geometry): add slopeToAngle

--- src/app/geometry/models/line.ts
+++ src/app/geometry/models/line.ts
@@ -5,14 +5,18 @@ export interface Line {
     b: number;
 }
 
-export function slop(segment: Segment): number {
+export function slope(segment: Segment): number {
     const {x: x1, y: y1} = segment[0];
-    const {x: x2, y: y2} = segment[0];
+    const {x: x2, y: y2} = segment[1];
     return (y2 - y1) / (x2 - x1);
 }
 
+export function slopeToAngle(slope: number): number {
+    return Math.atan(slope) * 180 / Math.PI;
+}
+
 export function lineEquation(segment: Segment): Line {
-    const a = slop(segment);
+    const a = slope(segment);
     const b = segment[0].y - a * (segment[0].x);
     return {a, b};
 }
