e826f64a29ef42fd8aa36657310848fb7058f454
Twazny <wazny.tomek@gmail.com>
2023-12-10 22:53:58 +0100

    feat(zoom): use new icon buttons

--- src/app/workspace/components/scale/scale.component.ts
+++ src/app/workspace/components/scale/scale.component.ts
@@ -1,11 +1,14 @@
 import { ChangeDetectionStrategy, Input, Output, EventEmitter } from "@angular/core";
 import { Component } from "@angular/core";
+import { ButtonComponent } from "src/app/ui/components/button/button.component";
+import { IconComponent } from "src/app/ui/components/icon/icon.component";
 
 @Component({
     selector: 'app-scale',
     templateUrl: './scale.component.html',
     styleUrls: ['./scale.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [ButtonComponent, IconComponent],
     standalone: true,
 })
 export class ScaleComponent {
