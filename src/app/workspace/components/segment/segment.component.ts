a2c1e6423f005880123459a8e3bd1e7309464626
Twazny <wazny.tomek@gmail.com>
2024-02-05 23:29:17 +0100

    feat(SegmentComponent): refactor ngOnChanges to signal inputs

--- src/app/workspace/components/segment/segment.component.ts
+++ src/app/workspace/components/segment/segment.component.ts
@@ -3,11 +3,11 @@ import {
   ChangeDetectionStrategy,
   Component,
   EventEmitter,
-  Input,
-  OnChanges,
+  InputSignal,
   Output,
-  WritableSignal,
+  computed,
   effect,
+  input,
   signal,
 } from '@angular/core';
 import {
@@ -25,17 +25,16 @@ import {
   imports: [NgClass],
   standalone: true,
 })
-export class SegmentComponent implements OnChanges {
-  @Input({ required: true }) segment!: Segment;
-  @Input({ required: true }) selected: boolean = false;
+export class SegmentComponent {
+  public segment: InputSignal<Segment> = input.required();
+  public selected: InputSignal<boolean> = input.required();
 
   @Output() public readonly hoveredChange: EventEmitter<boolean> =
     new EventEmitter();
 
-  protected path: string = '';
-  protected rotate: string = '';
-
-  protected hovered: WritableSignal<boolean> = signal(false);
+  protected path = computed(() => this._calculatePath(this.segment()));
+  protected rotate = computed(() => this._calculateRotate(this.segment()));
+  protected hovered = signal(false);
 
   private readonly hoveredEffect = effect(() =>
     this.hoveredChange.emit(this.hovered())
@@ -43,19 +42,24 @@ export class SegmentComponent implements OnChanges {
 
   private static readonly PROXIMITY_RADIUS = 10;
 
-  public ngOnChanges(): void {
-    const slopes = slope(this.segment);
+  private _calculateRotate(segment: Segment): string {
+    const slopes = slope(segment);
     const angle = slopeToAngle(slopes);
-    const len = getSegmentLength(this.segment);
-    const [p1, p2] = this.segment;
+    const [p1, p2] = segment;
     const p = p1.x > p2.x ? p2 : p1;
-    this.rotate = `rotate(${angle}, ${p.x}, ${p.y})`;
+    return `rotate(${angle}, ${p.x}, ${p.y})`;
+  }
+
+  private _calculatePath(segment: Segment): string {
+    const len = getSegmentLength(segment);
+    const [p1, p2] = segment;
+    const p = p1.x > p2.x ? p2 : p1;
     const r = SegmentComponent.PROXIMITY_RADIUS;
-    this.path = `
-        M ${p.x} ${p.y - r}
-        A ${r} ${r} 0 1 0 ${p.x} ${p.y + r}
-        l ${len} 0
-        A ${r} ${r} 0 1 0 ${p.x + len} ${p.y - r}
-        l -${len} 0`;
+    return `
+    M ${p.x} ${p.y - r}
+    A ${r} ${r} 0 1 0 ${p.x} ${p.y + r}
+    l ${len} 0
+    A ${r} ${r} 0 1 0 ${p.x + len} ${p.y - r}
+    l -${len} 0`;
   }
 }
